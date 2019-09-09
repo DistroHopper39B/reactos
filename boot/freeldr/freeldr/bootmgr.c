02f0bf72afd57f36625ad99dd49e0932ce05ba72
Mark Harmstone <mark@harmstone.com>
2019-09-09 19:45:42 +0100

    [FREELDR] Add "WindowsVista" boot type

--- boot/freeldr/freeldr/bootmgr.c
+++ boot/freeldr/freeldr/bootmgr.c
@@ -67,6 +67,7 @@ static const struct
 #endif
     {"Windows"     , EditCustomBootNTOS , LoadAndBootWindows},
     {"Windows2003" , EditCustomBootNTOS , LoadAndBootWindows},
+    {"WindowsVista", EditCustomBootNTOS , LoadAndBootWindows},
 };
 
 /* FUNCTIONS ******************************************************************/
