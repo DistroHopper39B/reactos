0b68baa18de79d676d6b180358efc9c01c97b052
Justin Miller <justin.miller@reactos.org>
2023-11-18 19:46:19 -0800

    [NTOS] Fix some locks

--- ntoskrnl/ke/freeze.c
+++ ntoskrnl/ke/freeze.c
@@ -85,6 +85,13 @@ KeFreezeExecution(IN PKTRAP_FRAME TrapFrame,
 #endif
 
 #ifdef CONFIG_SMP
+    /* Acquire lock */
+    while (InterlockedBitTestAndSet((PLONG)&KiFreezeExecutionLock, 0))
+    {
+        /* Loop until lock is free */
+        while ((*(volatile KSPIN_LOCK*)&KiFreezeExecutionLock) & 1);
+    }
+
     Prcb = KeGetCurrentPrcb();
     TargetAffinity = KeActiveProcessors;
     TargetAffinity &= ~Prcb->SetMember;
@@ -129,6 +136,10 @@ KeThawExecution(IN BOOLEAN Enable)
     Prcb = KeGetCurrentPrcb();
     TargetAffinity = KeActiveProcessors;
     TargetAffinity &= ~Prcb->SetMember;
+
+
+    /* Release lock */
+    InterlockedAnd((PLONG)&KiFreezeExecutionLock, 0);
 
     /* Loop through every processor */
     for (i = 0, Current = 1; i < KeNumberProcessors; i++, Current <<= 1)
