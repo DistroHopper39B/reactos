a1877dcd3bce65988eb344054b8d2f938846c4e4
DistroHopper39B <76637604+DistroHopper39B@users.noreply.github.com>
2025-05-26 01:54:45 -0400

    Update bootmgr.c

--- boot/freeldr/freeldr/bootmgr.c
+++ boot/freeldr/freeldr/bootmgr.c
@@ -59,9 +59,11 @@ OSLoadingMethods[] =
 #if defined(_M_IX86) || defined(_M_AMD64)
 
 #ifndef UEFIBOOT
+#ifndef SARCH_APPLETV
     {"Drive"       , EditCustomBootDisk      , LoadAndBootDevice},
     {"Partition"   , EditCustomBootPartition , LoadAndBootDevice},
     {"BootSector"  , EditCustomBootSectorFile, LoadAndBootDevice},
+#endif /* SARCH_APPLETV */
     {"Linux"       , EditCustomBootLinux, LoadAndBootLinux  },
 #endif /* UEFIBOOT */
 
