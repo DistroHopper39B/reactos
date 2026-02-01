ea4717aeff66506cf4126d932791fc5a98e90426
Sylas Hollander <76637604+DistroHopper39B@users.noreply.github.com>
2026-01-31 22:32:18 -0500

    [FREELDR:APPLETV] Add copyright authors of uefimem for appletvmem

--- boot/freeldr/freeldr/arch/i386/appletv/appletvmem.c
+++ boot/freeldr/freeldr/arch/i386/appletv/appletvmem.c
@@ -2,7 +2,8 @@
  * PROJECT:     FreeLoader
  * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
  * PURPOSE:     Hardware-specific creating a memory map routine for the original Apple TV
- * COPYRIGHT:   Copyright 2023-2026 Sylas Hollander (distrohopper39b.business@gmail.com)
+ * COPYRIGHT:   Authors of uefimem.c
+ *              Copyright 2023-2026 Sylas Hollander (distrohopper39b.business@gmail.com)
  */
 
 /* INCLUDES ******************************************************************/
